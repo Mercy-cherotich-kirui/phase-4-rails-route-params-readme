dec0bfa9cb3ab5cc7c5f4cbdaa9edaf5fb5b6263
mercy-cherotich-kirui <macivy49@gmail.com>
2023-06-28 19:51:45 +0300

    Done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -5,4 +5,8 @@ class CheesesController < ApplicationController
     render json: cheeses
   end
 
+  def show
+    cheese = Cheese.find(params[:cheese_id])
+    render json: cheese
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   get '/cheeses', to: 'cheeses#index'
+  get '/cheeses/:cheese_id', to: 'cheeses#show'
 end
